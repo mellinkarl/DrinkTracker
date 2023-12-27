56b9c025ea3ff28358647e4d892fa041552d1f7b
mellinkarl <mellinka@oregonstate.edu>
2023-12-27 10:16:15 -0800

    Initial Commit

---
+++ FirstWatchApp Watch App/FirstWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstWatchAppApp.swift
+//  FirstWatchApp Watch App
+//
+//  Created by Karl Mellinger on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
